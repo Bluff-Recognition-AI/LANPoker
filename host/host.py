2c0b3cc334e2c669ef61217d2fdd55f435e11b5c
Marcin Grawinski <mar.grawinski@gmail.com>
2024-05-13 09:29:59 +0200

    added host (new) as main file, up for discussion

--- host/host.py
+++ host/host.py
@@ -1,58 +1,57 @@
 from libs.web import json_server as s
-from libs.engine import engine
+from libs.engine import engine as eng
+from libs.engine import player
 
-import time
-import json
+
 import threading
 
 
-# game setup
-config = s.PokerConfig(500, 1000, 2000, 3)
-players = [engine.Player("Ricky", 10000), s.Player("Julian", 10000), s.Player("John", 10000)]
+def main():
+    # game setup
+    config = eng.PokerConfig(500, 1000, 2000, 3)
+    players = [player.Player("Ricky", 10000), player.Player("Julian", 10000), player.Player("John", 10000)]
 
-engine = engine.PokerEngine()
-engine.set_config(config)
+    engine = eng.PokerEngine()
+    engine.set_config(config)
 
-for player in players:
-    engine.add_player(player)
+    for player in players:
+        engine.add_player(player)
 
-engine.reset()
-engine.game_step()
+    engine.reset()
+    engine.game_step()
 
-# server setup
-HOST = '0.0.0.0'  # All available interfaces
-PORT = 5555  # Arbitrary port number
-server = s.JSONServer(HOST, PORT)
-server_thread = threading.Thread(target=server.start)
-server_thread.start()
+    # server setup
+    HOST = '0.0.0.0'  # All available interfaces
+    PORT = 5555  # Arbitrary port number
+    server = s.JSONServer(HOST, PORT)
+    server_thread = threading.Thread(target=server.start)
+    server_thread.start()
 
-input("if all the players connected press any button")
+    input("if all the players connected press any button")
 
-while(engine.running):
-    if(engine.game_phase != engine.GamePhase.WAITING_MOVE):
-        engine.game_step()
-    else:
+
+    while(engine.running):
+        if(engine.game_phase != eng.GamePhase.WAITING_MOVE):
+            engine.game_step()
+        else:
+            print(engine.get_game_state())
+            server.broadcast("Input move:")
+            # move_name = input()
+            move_name = server.wait_data()
+            print(move_name)
+
+            value = None
+            if(move_name == eng.MoveType.BET.value or move_name == eng.MoveType.RAISE.value):
+                server.broadcast("Input value: (min 4000)")
+                # value = int(input())
+                value = server.wait_data()
+            if value:
+                value = int(value)
+            move = eng.Move(eng.MoveType(move_name), value)
+            print(vars(move))
+            engine.game_step(move)
+
         print(engine.get_game_state())
-        server.broadcast(json.dumps("Input move:"))
-        # move_name = input()
-        while not server.client_data:
-            time.sleep(0.2)
-        move_name = json.loads(server.client_data[0])
-        print(move_name)
-        server.client_data.clear()
 
-        value = None
-        if(move_name == engine.MoveType.BET.value or move_name == engine.MoveType.RAISE.value):
-            server.broadcast(json.dumps("Input value: (min 4000)"))
-            # value = int(input())
-            while not server.client_data:
-                time.sleep(0.2)
-            value = json.loads(server.client_data[0])
-            server.client_data.clear()
-        if value:
-            value = int(value)
-        move = engine.Move(engine.MoveType(move_name), value)
-        print(vars(move))
-        engine.game_step(move)
-
-    print(engine.get_game_state())
+if __name__ == "__main__":
+    main()
