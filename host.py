00d7c31d05238221aefe37748874c0b148233f46
Marcin Grawinski <mar.grawinski@gmail.com>
2024-05-21 13:03:32 +0200

    first try connecting host, client and front-end

--- host.py
+++ host.py
@@ -3,7 +3,7 @@ from libs.engine.pokerengine.player import *
 from libs.web.socketserverclient.json_server import *
 import threading
 
-PLAYER_COUNT = 4
+PLAYER_COUNT = 1
 
 
 def init_engine(players: list) -> PokerEngine:
@@ -27,51 +27,30 @@ def init_server() -> JSONServer:
     return server
 
 def main():
-    players = [Player("Marcin", 10000), Player("Szymon", 10000), Player("Julia", 10000), Player("Wiktor", 10000)]
-    engine = init_engine(players)
-    
     server = init_server()
-
     print("wait for the players to connect")
+    players = []
     while len(server.clients) != PLAYER_COUNT:
+        data = server.wait_data()
+        if "name" in data:
+            players.append(Player(data["name"], 10000))
         time.sleep(0.1)
     
-    player_connection = {}
-    for i in range(PLAYER_COUNT):
-        server.send_to([players[i].name, i], server.clients[i])     # sends to each player his name and client info
-        player_connection[players[i]] = server.clients[i]           
+    engine = init_engine(players)
+    server.broadcast(engine.get_game_state())          
  
-    server.broadcast("\nGAME STARTS\n")
-    time.sleep(1)
+    time.sleep(0.1)
     while(engine.running):
         if(engine.game_phase != GamePhase.WAITING_MOVE):
             engine.game_step()
         else:
-            info = engine.get_game_state() 
-            for key in info:
-                print(f"{key} : {info[key]}")
-            server.broadcast({"gamestate": info})
-            server.send_to("Input move:", player_connection[players[engine.get_game_state()["turn"]]])
-            move_name = server.wait_data()
-
-            value = None
-            if(move_name == MoveType.BET.value or move_name == MoveType.RAISE.value):
-                server.send_to("Input value: (min 4000)", player_connection[players[engine.get_game_state()["turn"]]])
-                value = server.wait_data()
-            if value:
-                value = int(value)
-            move = Move(MoveType(move_name), value)
+            move_get = server.wait_data()
+            move = Move(MoveType(move_get["name"]), move_get["value"])
             print(vars(move))
-            server.broadcast({"playermove": [move_name, value]})
 
             engine.game_step(move)
-
-
-        info = engine.get_game_state() 
-        for key in info:
-            print(f"{key} : {info[key]}")
-        server.broadcast({"gamestate": info})
-        time.sleep(1)
+        server.broadcast(engine.get_game_state())
+        time.sleep(0.1)
     
 
 if __name__ == "__main__":
